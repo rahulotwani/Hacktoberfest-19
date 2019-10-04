c9ff974c5afd38ae3e74a5a22c9bc2675b0be54a
sakshi gaur <44137767+gaursakshi@users.noreply.github.com>
2019-10-04 14:23:50 +0530

    Create hello_world_gaursakshi.java

---
+++ scripts/hello_world_gaursakshi.java
@@ -0,0 +1,14 @@
+// LANGUAGE: Java
+// ENV: Node.JRE 
+// AUTHOR: gaursakshi
+// GITHUB: https://github.com/gaursakshi
+
+class hello_world 
+{ 
+    // Your program begins with a call to main(). 
+    // Prints "Hello, World" to the terminal window. 
+    public static void main(String args[]) 
+    { 
+        System.out.println("Hello, World"); 
+    } 
+}
